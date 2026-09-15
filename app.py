4ca591cfd999eafb2e7790310d0af998f2f09bd3
HoruzGiltech <giltechnology.2025@gmail.com>
2026-09-15 12:46:28 -0400

    feat: agregar limites de recursos y endpoint /memory-leak

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+memory_hog=[]
+
 @app.route('/')
 def home():
     return jsonify({
@@ -14,6 +16,16 @@ def home():
 @app.route('/crash')
 def crash():
     os._exit(1)
+
+@app.rout('memory-leak')
+def memory_leak():
+    dummy_data='X' *(30*1024*1024)
+    memory_hog.append(dummy_data)
+    return jsonify({
+        "status": "leaking",
+        "allocated_blocks":len(memory_hog),
+        "message":"Consumiendo 30MB adicionales de memoria RAM ...🧠💥 "
+    })
 
 
 if __name__ == '__main__':
